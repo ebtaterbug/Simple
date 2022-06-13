8c819bc643a8cd1033905682fc34bfc97f158d4c
Eric Furukawa <eric.furukawa@gmail.com>
2022-06-13 13:24:04 -0700

    added components and sample flashcards

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -9,23 +9,35 @@ function App() {
   }
   
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>CODE</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button onClick={alertFunc}>Add an alert</Button>
-      </header>
-    </div>
+    <h1>Hello World!</h1>
   );
 }
+
+const sampleCards = [
+  {
+    id: 1,
+    question: 'What color is the sky?',
+    answer: 'blue',
+    options: ['yellow', 'blue', 'red', 'purple']
+  },
+  {
+    id: 2,
+    question: 'How many ounces are in a pound?',
+    answer: '16',
+    options: ['12', '14', '16', '18']
+  },
+  {
+    id: 3,
+    question: 'What type of dog is Snoopy?',
+    answer: 'beagle',
+    options: ['beagle', 'lab', 'collie', 'pitbull']
+  },
+  {
+    id: 4,
+    question: 'How many fucks do I give?',
+    answer: '0',
+    options: ['0', '1', '2', '76']
+  },
+]
 
 export default App;
