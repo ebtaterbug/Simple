4c8cba314d8c66282fd7de5d20bec3dfc0c102cf
Eric Furukawa <eric.furukawa@gmail.com>
2022-06-14 15:59:39 -0700

    added styling to flashcard component

--- client/src/components/Flashcard.js
+++ client/src/components/Flashcard.js
@@ -4,8 +4,16 @@ export default function Flashcard({ flashcard }) {
 const [flip, setFlip] = useState(false);
 
   return (
-    <div onClick={() => setFlip(!flip)}>
-        {flip ? flashcard.answer : flashcard.question}
+
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="card">
+          <div className="card-body" onClick={() => setFlip(!flip)}>
+            {flip ? flashcard.answer : flashcard.question}
+          </div>
+        </div>
+      </div>
     </div>
+    
   )
 }
